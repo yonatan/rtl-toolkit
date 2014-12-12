21bd22e5c184eb6603c03eb700fcf7de361438c2
Yonatan Offek <yonatan.offek@gmail.com>
2014-12-12 21:58:23 +0200

    Avoid deprecated $.browser object.

--- rtlx/rtlx.js
+++ rtlx/rtlx.js
@@ -17,7 +17,21 @@
 
     $(function() {
         // IE8 doesn't support 4-value background positions, so we have to measure and flip in realtime using javascript.
-        if($.browser.msie && ($.browser.version.indexOf("8.") == 0)) {
+        function getInternetExplorerVersion()
+        // Returns the version of Internet Explorer or a -1
+        // (indicating the use of another browser).
+        {
+            var rv = -1; // Return value assumes failure.
+            if (navigator.appName == 'Microsoft Internet Explorer')
+            {
+                var ua = navigator.userAgent;
+                var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+                if (re.exec(ua) != null)
+                    rv = parseFloat( RegExp.$1 );
+            }
+            return rv;
+        }
+        if((getInternetExplorerVersion() | 0) == 8) {
             setTimeout(function(){ flipBg($("*")); }, 200);
         }
     });
